0286165049fcff781a0cb7cbebac63d709754a76
danjel <danjel@pop-os.localdomain>
2021-01-04 17:32:06 +0100

    first init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./styles/app.scss";
+
+import Player from "./components/Player";
+import Song from "./components/Song";
+
+import data from "./util";
+
+function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
+    </div>
+  );
+}
+
+export default App;
