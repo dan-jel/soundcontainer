7d7b53e4f1d13fe7b86dc261ae68e1872dc5245b
dan-jel <daniel.heese1@gmail.com>
2021-01-05 14:22:57 +0100

    more songs + color slider

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ function Archiv() {
         "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
       id: uuidv4(),
       active: true,
-      color: ["#5D3933", "#9DB555"],
+      color: ["#B7C184", "#BCD4D7"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=11246",
     },
     {
@@ -31,6 +31,86 @@ function Archiv() {
       active: false,
       color: ["#ADD9B7", "#468866"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9925",
+    },
+    {
+      name: "False Hope",
+      artist: "Nymano, Pandrezz",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/09fb436604242df99f84b9f359acb046e40d2e9e-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#966994", "#5A5281"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10148",
+    },
+    {
+      name: "Seascape",
+      artist: "Makzo",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C28188", "#4D62A1"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11773",
+    },
+    {
+      name: "Les Mouvements d'Hiver",
+      artist: "L'Indécis, sadtoi",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/d2898bc0ef36b5d67d6793062d588d208a90421c-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E1B2CA", "#715A86"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10351",
+    },
+    {
+      name: "Bliss",
+      artist: "Misha, Jussi Halme",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#2A416D", "#AF8AAB"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+    },
+    {
+      name: "Creswick",
+      artist: "Aviino",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#B062B6", "#3E7DBF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10454",
+    },
+    {
+      name: "Flushing the Stairs",
+      artist: "Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/6defa6c89a6902fdd84bccd720c2b8fd29c40990-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#065F51", "#9BBB82"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9917",
+    },
+    {
+      name: "Colors Fade",
+      artist: "Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/686808356b2bbc6a368e372ea604150bc346c788-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#CAE4E6", "#55A2B6"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10023",
+    },
+    {
+      name: "Paraglider",
+      artist: "Kendall Miles, H E R B",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/4fac6bb3a32f9e76b465990ba8d97d7db8a372f5-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FCB25C", "#B96D62"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9318",
     },
   ];
 }
